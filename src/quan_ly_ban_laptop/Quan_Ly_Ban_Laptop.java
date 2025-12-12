a5367677186c05bc7ced280dbdcc77c812c39214
dtd14 <daothanhdat093@gmail.com>
2025-12-12 19:43:49 +0700

    new

---
+++ src/quan_ly_ban_laptop/Quan_Ly_Ban_Laptop.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quan_ly_ban_laptop;
+
+/**
+ *
+ * @author Admin
+ */
+public class Quan_Ly_Ban_Laptop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
